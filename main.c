9b4de5030d440857281843ddb5481b0852998d5d
joaovitorleitzkedecampos <jvldecampos@gmail.com>
2025-08-13 11:13:51 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    printf ("Calculo do IMC \n");
+    printf ("Altura: %f \n", 1.64);
+    printf ("Peso : %f \n", 62.5);
+    
+    printf ("IMC: %f ",62.5 / (1.64* 1.64));
+    return 0;
+}
