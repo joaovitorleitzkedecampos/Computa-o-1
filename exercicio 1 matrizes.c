ca6a71c4a325767cbc9307038c0b36711f3cc322
joaovitorleitzkedecampos <jvldecampos@gmail.com>
2025-11-11 13:53:48 -0300

    Refactor matrix string handling and add vowel/consonant checks
    Added functions to check for vowels and consonants, and modified string handling in the matrix.

--- exercicio 1 matrizes.c
+++ exercicio 1 matrizes.c
@@ -1,67 +1,68 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
 #define LINHAS 3
 #define COLUNAS 3
 #define TAM_STRING 50
 
+
+int ehVogal(char c) {
+    c = tolower(c);
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
+
+
+int ehConsoante(char c) {
+    c = tolower(c);
+    return (isalpha(c) && !ehVogal(c));
+}
+
+
+void inverterString(char str[]) {
+    int i, j;
+    char temp;
+    for (i = 0, j = strlen(str) - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
+
 int main() {
     char matriz[LINHAS][COLUNAS][TAM_STRING];
-    char primeiraLetra, ultimaletra, pl, ul;
-    char str[100];
-    
-    printf("Preencha a matriz 3x3 com strings (max 50 caracteres cada):");
-    for(int i = 0; i < LINHAS; i++) {
-        for(int j = 0; j < COLUNAS; j++) {
+
+    printf("Preencha a matriz 3x3 com strings (máx 50 caracteres cada):\n");
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
             printf("Digite a string para a posição [%d][%d]: ", i, j);
-            
             fgets(matriz[i][j], TAM_STRING, stdin);
 
-           
-            size_t len = 0;
-            while (matriz[i][j][len] != '\0') len++;
-            if (len > 0 && matriz[i][j][len - 1] =='\0') {
+            
+            size_t len = strlen(matriz[i][j]);
+            if (len > 0 && matriz[i][j][len - 1] == '\n') {
                 matriz[i][j][len - 1] = '\0';
+                len--; 
+            }
+
+           
+            if (len > 0) {
+                
+                if (ehVogal(matriz[i][j][0]) && ehConsoante(matriz[i][j][strlen(matriz[i][j]) - 1])) {
+                    inverterString(matriz[i][j]);
+                }
             }
         }
     }
 
     
-    printf("Matriz preenchida:");
-    for(int i = 0; i < LINHAS; i++) {
-        for(int j = 0; j < COLUNAS; j++) {
+    printf("\nMatriz resultante:\n");
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
             printf("%s\t", matriz[i][j]);
         }
-        
+        printf("\n");
     }
-void inverterString(char str[]) {
-    int inicio = 0;
-    int fim = strlen(str) - 1;
-    char temp;
-    
-   
-    while (inicio < fim) {
-        temp = str[inicio];
-        str[inicio] = str[fim];
-        str[fim] = temp;
-        inicio++;
-        fim--;
-    }
-}
-
-    fgets(str, sizeof(str), stdin); 
-
-    
-    str[strcspn(str, "")] = 0; 
-
-    if (primeiraLetra == 'a' || primeiraLetra == 'e' || primeiraLetra == 'i' || 
-        primeiraLetra == 'o' || primeiraLetra == 'u') {
-        printf("A string começa com uma vogal.");
-        }
-        
-        inverterString(str);
-        printf("String invertida: %s", str);
-
-    
 
     return 0;
 }
